b066abe07a9cab0c53d89a8cd436b730008e393b
Caro-L-dev <liardcar@gmail.com>
2021-06-10 17:25:07 +0200

    :hammer: Fetch datas

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+/* First-reading on the page, labels are empty */
+    $firstname = $name = $email = $phone = $message = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $firstname = $_POST["firstname"];
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $message = $_POST["message"];
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -21,40 +33,41 @@
 
         <div class="form-container">
             <div class="cases">
-                <form id="contact-form" method="post" action="" role="form">
+                <!-- Second reading, when we clic on submit button : the page return the if condition -->
+                <form id="contact-form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" role="form">
                   
                         <div class="form-element">
                             <label for="firstname">Prénom <span class="important-infos">*</span></label>
-                            <input type="text" id="firstname" name="firstname" class="form-control" placeholder="Votre prénom">
+                            <input type="text" id="firstname" name="firstname" class="form-control" placeholder="Votre prénom" 
+                            value="<?php echo $firstname; ?>">
                             <p class="comments">Message d'erreur</p>
                         </div>
     
                         <div class="form-element">
                             <label for="name">Nom <span class="important-infos">*</span></label>
-                            <input type="text" id="name" name="name" class="form-control" placeholder="Votre nom">
+                            <input type="text" id="name" name="name" class="form-control" placeholder="Votre nom"
+                            value="<?php echo $name; ?>">
                             <p class="comments">Message d'erreur</p>
                         </div>
                    
 
                         <div class="form-element">
                             <label for="email">Email <span class="important-infos">*</span></label>
-                            <input type="text" id="email" name="email" class="form-control" placeholder="Votre email">
+                            <input type="text" id="email" name="email" class="form-control" placeholder="Votre email"
+                            value="<?php echo $email; ?>">
                             <p class="comments">Message d'erreur</p>
                         </div>
     
                         <div class="form-element">
                             <label for="phone">Téléphone <span class="important-infos"></span></label>
-                            <input type="text" id="phone" name="phone" class="form-control" placeholder="Votre téléphone">
+                            <input type="text" id="phone" name="phone" class="form-control" placeholder="Votre téléphone"
+                            value="<?php echo $phone; ?>">
                             <p class="comments">Message d'erreur</p>
                         </div>
-                  
-                  
-
-                   
-
+ 
                     <div class="form-element-message">
                         <label for="message">Message <span class="important-infos">*</span></label>
-                        <textarea id="message" nam="message" class="form-control" placeholder="Votre message"></textarea>
+                        <textarea id="message" nam="message" class="form-control" placeholder="Votre message"><?php echo $message; ?></textarea>
                         <p class="comments">Message d'erreur</p>
                     </div>
 
@@ -64,14 +77,10 @@
 
                   
                     <input type="submit" class="form-btn" value="Envoyer">
-                   
-                    
-
 
                    <p class="thank-you">Votre message a bien été envoyé.</p>
 
                 </form>
-
             </div>
         </div>
             
