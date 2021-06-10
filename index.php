d7cde6c547d720ea2e44ed07264ae65d8f793a27
Caro-L-dev <liardcar@gmail.com>
2021-06-10 18:47:11 +0200

    :hammer: Server side validation with error messages

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <?php
 /* First-reading on the page, labels are empty */
     $firstname = $name = $email = $phone = $message = "";
+    $firstnameError = $nameError = $emailError = $phoneError = $messageError = "";
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $firstname = verifyInput($_POST["firstname"]);
         $name = verifyInput ($_POST["name"]);
         $email = verifyInput ($_POST["email"]);
         $phone = verifyInput ($_POST["phone"]);
         $message = verifyInput ($_POST["message"]);
+
+        /* Server-side validation with error messages */
+        if(empty($firstname)) {
+            $firstnameError = "Je veux connaitre ton prénom !";
+        }
+
+        if(empty($name)) {
+            $nameError = "Ton nom m'intéresse aussi, tu ne t'échapperas pas !";
+        }
+
+        if(empty($message)) {
+            $messageError = "Que veux tu me dire ?";
+        }
+
+        
+
     }
 
     function verifyInput($var) {
@@ -47,39 +65,40 @@
                 <!-- htmlspecialchars can protects against xss flaws-->
                 <form id="contact-form" method="post" action="<?php echo htmlspecialchars ($_SERVER['PHP_SELF']); ?>" role="form">
                   
+                <!--Client-side validation with "required" in input-->
                         <div class="form-element">
                             <label for="firstname">Prénom <span class="important-infos">*</span></label>
-                            <input type="text" id="firstname" name="firstname" class="form-control" placeholder="Votre prénom" 
+                            <input type="text" id="firstname" name="firstname"  class="form-control" placeholder="Votre prénom" 
                             value="<?php echo $firstname; ?>">
-                            <p class="comments">Message d'erreur</p>
+                            <p class="comments"><?php echo $firstnameError; ?></p>
                         </div>
     
                         <div class="form-element">
                             <label for="name">Nom <span class="important-infos">*</span></label>
                             <input type="text" id="name" name="name" class="form-control" placeholder="Votre nom"
                             value="<?php echo $name; ?>">
-                            <p class="comments">Message d'erreur</p>
+                            <p class="comments"><?php echo $nameError; ?></p>
                         </div>
                    
 
                         <div class="form-element">
                             <label for="email">Email <span class="important-infos">*</span></label>
-                            <input type="text" id="email" name="email" class="form-control" placeholder="Votre email"
+                            <input type="email"  id="email" name="email" class="form-control" placeholder="Votre email"
                             value="<?php echo $email; ?>">
-                            <p class="comments">Message d'erreur</p>
+                            <p class="comments"></p>
                         </div>
     
                         <div class="form-element">
                             <label for="phone">Téléphone <span class="important-infos"></span></label>
-                            <input type="text" id="phone" name="phone" class="form-control" placeholder="Votre téléphone"
+                            <input type="tel" id="phone" name="phone" class="form-control" placeholder="Votre téléphone"
                             value="<?php echo $phone; ?>">
-                            <p class="comments">Message d'erreur</p>
+                            <p class="comments"></p>
                         </div>
  
                     <div class="form-element-message">
                         <label for="message">Message <span class="important-infos">*</span></label>
                         <textarea id="message" nam="message" class="form-control" placeholder="Votre message"><?php echo $message; ?></textarea>
-                        <p class="comments">Message d'erreur</p>
+                        <p class="comments"><?php echo $messageError; ?></p>
                     </div>
 
                     <p class="important-infos">
